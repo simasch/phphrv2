c805b4ae9a14f2be89110a9ccf3606ac3da21e85
simasch <smtve,01>
2016-11-27 21:55:35 +0100

    added 404 example

--- src/public/index.php
+++ src/public/index.php
@@ -29,30 +29,13 @@ $container['logger'] = function ($c) {
 
 $container['db'] = function ($c) {
     $db = $c['settings']['db'];
-    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
-        $db['user'], $db['pass']);
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'], $db['user'], $db['pass']);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     return $pdo;
 };
 
 $container['view'] = new \Slim\Views\PhpRenderer("../templates/");
-
-$app->get('/hello/{name}', function (Request $request, Response $response) {
-    $this->logger->addInfo("Something interesting happened");
-
-    $name = $request->getAttribute('name');
-
-    $stmt = $this->db->prepare("SELECT id, name FROM person WHERE id = 1");
-    $stmt->execute();
-
-    $row = $stmt->fetch();
-    $this->logger->addInfo($row["id"] . $row["name"]);
-
-    $response->getBody()->write("Hello, $name");
-
-    return $response;
-});
 
 $app->get('/person', function (Request $request, Response $response) {
     $this->logger->addInfo("People list");
@@ -67,7 +50,11 @@ $app->get('/person/{id}', function (Request $request, Response $response, $args)
     $dao = new PersonDao($this->db);
     $person = $dao->find($id);
 
-    return $this->view->render($response, "person.phtml", ["person" => $person]);
+    if ($person->getId() == null) {
+        return $response->withStatus(404)->write('Person not found');
+    } else {
+        return $this->view->render($response, "person.phtml", ["person" => $person]);
+    }
 });
 
 $app->post('/person/{id}', function (Request $request, Response $response, $args) {
