5e957300d1d8dcd45583fe44ca128ed5698ef196
simasch <smtve,01>
2016-11-27 20:39:49 +0100

    initial commit

---
+++ src/public/index.php
@@ -0,0 +1,55 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+require '../vendor/autoload.php';
+
+spl_autoload_register(function ($classname) {
+    require("../classes/" . $classname . ".php");
+});
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host'] = "localhost";
+$config['db']['user'] = "root";
+$config['db']['pass'] = "";
+$config['db']['dbname'] = "hr";
+
+$app = new \Slim\App(["settings" => $config]);
+
+$container = $app->getContainer();
+
+$container['logger'] = function ($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("../logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+$app->get('/hello/{name}', function (Request $request, Response $response) {
+    $this->logger->addInfo("Something interesting happened");
+
+    $name = $request->getAttribute('name');
+
+    $stmt = $this->db->prepare("SELECT id, name FROM person WHERE id = 1");
+    $stmt->execute();
+
+    $row = $stmt->fetch();
+    $this->logger->addInfo($row["id"] . $row["name"]);
+
+    $response->getBody()->write("Hello, $name");
+
+    return $response;
+});
+
+$app->run();
